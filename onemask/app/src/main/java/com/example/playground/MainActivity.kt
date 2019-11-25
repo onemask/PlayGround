8812db6e4ef3f9f5d0d242862618f2133e98db4e
김수연 <onemask@naver.com>
2019-11-26 00:23:53 +0900

    버튼 클릭 이벤트 리스너 추가

--- onemask/app/src/main/java/com/example/playground/MainActivity.kt
+++ onemask/app/src/main/java/com/example/playground/MainActivity.kt
@@ -3,23 +3,31 @@ package com.example.playground
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import com.example.playground.state.CounterState
 
 class MainActivity : AppCompatActivity() {
 
-    private val decremetnButton by lazy { findViewById<Button>(R.id.decrement_button) }
+    private val decrementButton by lazy { findViewById<Button>(R.id.decrement_button) }
     private val incrementButton by lazy { findViewById<Button>(R.id.increment_button) }
-    private val counterText by lazy { findViewById<Button>(R.id.counter_text_view) }
+    private val counterText by lazy { findViewById<TextView>(R.id.counter_text_view) }
 
+    private var state = CounterState()
+    set(value) {
+        field = value
+        counterText.text = "${state.value}"
+    }
     //For Counting
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        decremetnButton.setOnClickListener {
-
+        decrementButton.setOnClickListener {
+            state = state.copy(value = state.value-1)
         }
 
         incrementButton.setOnClickListener {
+            state = state.copy(value = state.value+1)
 
         }
     }
