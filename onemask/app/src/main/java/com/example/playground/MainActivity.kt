ba5b9bd48211beaf6433d23f86cc16bb1301c0f4
김수연 <onemask@naver.com>
2019-11-26 00:12:31 +0900

    add MainActivity

--- onemask/app/src/main/java/com/example/playground/MainActivity.kt
+++ onemask/app/src/main/java/com/example/playground/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    private val decremetnButton by lazy { findViewById<Button>(R.id.decrement_button) }
+    private val incrementButton by lazy { findViewById<Button>(R.id.increment_button) }
+    private val counterText by lazy { findViewById<Button>(R.id.counter_text_view) }
+
+    //For Counting
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decremetnButton.setOnClickListener {
+
+        }
+
+        incrementButton.setOnClickListener {
+
+        }
     }
 }
