e77cefeeb31fe25688e098ce121afb24fec815aa
soo <onemask@stunitas.com>
2019-11-26 17:27:50 +0900

    Add Reducer.kt

---
+++ onemask/app/src/main/java/com/example/playground/reducer/Reducer.kt
@@ -0,0 +1,16 @@
+package com.example.playground.reducer
+
+import com.example.playground.action.Action
+import com.example.playground.action.CounterActions
+import com.example.playground.state.CounterState
+
+typealias Reducer<S> = (S, Action) -> S
+
+val CounterStateReducer: Reducer<CounterState> = { old, action ->
+    when (action) {
+        is CounterActions.Init -> CounterState()
+        is CounterActions.Increment -> old.copy(value = old.value + 1)
+        is CounterActions.decrement -> old.copy(value = old.value - 1)
+        else -> old
+    }
+}
