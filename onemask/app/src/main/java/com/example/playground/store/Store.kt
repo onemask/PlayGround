19439e1e75e2904f1ad5fc3b3bca553b00571784
soo <onemask@stunitas.com>
2019-11-26 19:48:00 +0900

    Add Store interface

---
+++ onemask/app/src/main/java/com/example/playground/store/Store.kt
@@ -0,0 +1,16 @@
+package com.example.playground.store
+
+import com.example.playground.action.Action
+import com.example.playground.state.State
+
+
+typealias StoreSubscriber <S> = (S) -> Unit
+
+interface Store<S : State> {
+    fun dispatch(action: Action)
+    fun add(subscriber: StoreSubscriber<S>): Boolean
+    fun remove(subscriber: StoreSubscriber<S>): Boolean
+}
+
+
+
