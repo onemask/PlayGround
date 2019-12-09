b0d89a3f6a25b1f2c0628ef9eeae0d03972b1e6d
soo <onemask@stunitas.com>
2019-12-09 16:40:14 +0900

    Add FoldRight & Fold Logic

--- onemask/app/src/main/java/com/example/playground/MainActivity.kt
+++ onemask/app/src/main/java/com/example/playground/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.example.playground.action.CounterActions
@@ -19,18 +20,70 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         DI.store.add {
-            counterText.text ="${it.value}"
+            counterText.text = "${it.value}"
         }
 
         DI.store.dispatch(action = CounterActions.Init)
 
         decrementButton.setOnClickListener {
-            DI.store.dispatch(action =  CounterActions.Decrement)
+            DI.store.dispatch(action = CounterActions.Decrement)
         }
 
         incrementButton.setOnClickListener {
-            DI.store.dispatch(action =  CounterActions.Increment)
+            DI.store.dispatch(action = CounterActions.Increment)
         }
+
+
+        /*
+         * foldRight & fold
+         */
+        val list = listOf(1, 2, 3, 4, 5)
+        val sumTotal = list.foldRight(list.first(), { total, next ->
+            println("total $total newxt $next")
+            total + next
+        })
+        println("sumTotal $sumTotal")
+
+        println("fold \n")
+        val sumLeftTotal = list.fold(0, { total, next ->
+            println("total $total newxt $next")
+            total + next
+        })
+        println("sumLeftTotal $sumLeftTotal")
+
+
+
+        data class Fruit(
+            val price: Int,
+            val name: String
+        )
+
+        /*
+        * foldRightIndexed & foldIndexed
+        */
+        val objectLlist =
+            listOf(
+                Fruit(100, "apple"),
+                Fruit(200, "pineapple"),
+                Fruit(500, "watermelon")
+            )
+
+        println("foldRightIndexed \n")
+        val fruitList = objectLlist.foldRightIndexed(0, { index, fruitPrice, next ->
+            println("\n index $index fruitPrice $fruitPrice next $next ")
+            fruitPrice.price + next
+        })
+        println("fruitList $fruitList")
+
+
+        println("foldIndex \n")
+        val fruitLeftList =
+            objectLlist.foldIndexed(objectLlist.first().name, { index, fruitName, next ->
+                println("\n index $index fruitName $fruitName next $next ")
+                fruitName + " " + next.name
+            })
+        println("fruitLeftList $fruitLeftList")
+
 
     }
 }
