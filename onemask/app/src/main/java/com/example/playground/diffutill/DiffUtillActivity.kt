7e72f45bedb3cfeaacb72e9ba9704fdd7e5bb2c3
김수연 <onemask@naver.com>
2020-05-03 21:50:34 +0900

    add DiffUtillActivity

---
+++ onemask/app/src/main/java/com/example/playground/diffutill/DiffUtillActivity.kt
@@ -0,0 +1,12 @@
+package com.example.playground.diffutill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiffUtillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diff_utill)
+    }
+}
