b687eabaf88bfea6721eef7be0b26e6ea5239ed0
김수연 <onemask@naver.com>
2020-05-24 15:43:50 +0900

    delete TileAdapterModel

--- onemask/app/src/main/java/com/example/playground/diffutill/adapter/model/TileAdapterModel.kt
+++ onemask/app/src/main/java/com/example/playground/diffutill/adapter/model/TileAdapterModel.kt
@@ -1,3 +1,4 @@
+/*
 package com.example.playground.diffutill.adapter.model
 
 import androidx.recyclerview.widget.DiffUtil
@@ -18,7 +19,7 @@ class TileAdapterModel(private val adapter: RecyclerView.Adapter<RecyclerView.Vi
     fun size(): Int = tiles.size
 
     fun get(position: Int) = tiles[position]
-
+    //size 26
     private fun calDiff(newTiles: MutableList<Tile>) {
         val tileDiffUtilCallback = TileDiffUtilCallback(tiles, newTiles)
         val diffResult: DiffUtil.DiffResult = DiffUtil.calculateDiff(tileDiffUtilCallback)
@@ -38,10 +39,10 @@ class TileAdapterModel(private val adapter: RecyclerView.Adapter<RecyclerView.Vi
         setNewTiles(newTiles)
     }
 
-    fun eraseOneTile() {
+    private fun eraseOneTile() {
         val newTiles = mutableListOf<Tile>()
         tiles.isNotEmpty().let {
-            val erasedRandomIdx = (Random.nextDouble() * newTiles.size).toInt()
+            val erasedRandomIdx = (Random.nextDouble() * newTiles.size).toInt()//0
             tiles.forEachIndexed { index, tile ->
                 if (index != erasedRandomIdx) newTiles.add(tile)
             }
@@ -53,7 +54,7 @@ class TileAdapterModel(private val adapter: RecyclerView.Adapter<RecyclerView.Vi
     fun addOneTile() {
         val newTiles = mutableListOf<Tile>()
         newTiles.addAll(tiles)
-
+//idx 7 number 7
         val insertRandomIdx = (Random.nextDouble() * newTiles.size).toInt()
         newTiles.add(insertRandomIdx, Tile(tiles.size + 1))
 
@@ -77,7 +78,7 @@ class TileAdapterModel(private val adapter: RecyclerView.Adapter<RecyclerView.Vi
     fun addThreeTile() {
         val newTiles = mutableListOf<Tile>()
         newTiles.addAll(tiles)
-        repeat(3){
+        repeat(3) {
             val insertRandomIdx = (Random.nextDouble() * newTiles.size).toInt()
             newTiles.add(insertRandomIdx, Tile(tiles.size + 1))
         }
@@ -85,29 +86,9 @@ class TileAdapterModel(private val adapter: RecyclerView.Adapter<RecyclerView.Vi
         setNewTiles(newTiles)
     }
 
-    inner class TileDiffUtilCallback(
-        private val oldTiles: MutableList<Tile>,
-        private val newTiles: MutableList<Tile>
-    ) : DiffUtil.Callback() {
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldTiles[oldItemPosition] == newTiles[newItemPosition]
-        }
-
-        override fun getOldListSize(): Int {
-            return oldTiles.size
-        }
-
-        override fun getNewListSize(): Int {
-            return newTiles.size
-        }
-
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldTiles[oldItemPosition].number == newTiles[newItemPosition].number
-        }
-    }
 
     companion object {
         const val TILE_SIZE = 25
     }
 
-}
+}*/
